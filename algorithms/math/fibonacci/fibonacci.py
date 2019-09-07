641eca70c7419998d60a0bde3be64637234cbf0d
Demir Antay <demir99antay@gmail.com>
2019-09-07 16:02:57 +0300

    added fibonacci and its tests

--- algorithms/math/fibonacci/fibonacci.py
+++ algorithms/math/fibonacci/fibonacci.py
@@ -1,5 +1,12 @@
-def fibonacci():
+def fibonacci(number):
     '''
+    this function returns you a list of fibonacci sequenced elements. The length
+    of the array is determined by the number given in the parameter
+    '''
+    fib_array = [0, 1]
 
-    '''
-    pass
+    for i in range(0, number - 2, 1):
+        new_member = fib_array[i] + fib_array[i + 1]
+        fib_array.append(new_member)
+
+    return fib_array
