e08796e01cb60b097e566668befee6cb512476b6
Demir Antay <demir99antay@gmail.com>
2019-12-18 16:43:33 +0300

    added primality test and moved docs folder into their own modules

---
+++ python/data-structures/hash-table/hash_table.py
@@ -0,0 +1,12 @@
+def hashing():
+    pass
+
+
+class HashTable:
+    """
+    Hash tables are a type of data structure in which the address or the index
+    value of the data element is generated from a hash function. That makes
+    accessing the data faster as the index value behaves as a key for the data
+    value. In other words Hash table stores key-value pairs but the key is
+    generated through a hashing function.
+    """
