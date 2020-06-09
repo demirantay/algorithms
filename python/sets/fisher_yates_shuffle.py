66578a41c5d63432cfc3bf4143326e50c41c4357
Demir Antay <demir99antay@gmail.com>
2020-06-09 17:53:47 +0300

    finished fisher yates

--- python/sets/fisher_yates_shuffle.py
+++ python/sets/fisher_yates_shuffle.py
@@ -1,4 +1,24 @@
-def fisher_yates_shuffle():
+import random
+
+
+def fisher_yates_shuffle(set):
+    """
+    The Fisher–Yates shuffle is an algorithm for generating a random
+    permutation of a finite sequence—in plain terms, the algorithm shuffles
+    the sequence. The algorithm effectively puts all the elements into a hat;
+    it continually determines the next element by randomly drawing an element
+    from the hat until no elements remain. The algorithm produces an unbiased
+    permutation: every permutation is equally likely. The modern version of the
+    algorithm is efficient: it takes time proportional to the number of items
+    being shuffled and shuffles them in place
+
+    Basically it just fucking shuffles a set.
     """
 
-    """
+    for i in range(0, len(set), 1):
+        random_index = random.randint(0, 9)
+        temp = set[i]
+        set[i] = set[random_index]
+        set[random_index] = temp
+
+    return set
